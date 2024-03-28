856fc364362a9141132ccc66264ebfc96c0df5b6
Aviv Turgeman <avivturgeman10@gmail.com>
2024-03-28 15:06:32 +0200

    jjjj

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,7 +92,7 @@ export default function Home() {
         <div className="home-inner-left">
           <h1 className="text-style-hello">Hello! </h1>
           <h1 className="text-style-display">I am Aviv Turgeman, </h1>
-          <h1 className="text-style-end">A Practical Software Engineer</h1>
+          <h1 className="text-style-end">A Practical Softwaaaaaaaaaaaaaaaare Engineer</h1>
         </div>
         <div className="home-inner-right">
           <Image
